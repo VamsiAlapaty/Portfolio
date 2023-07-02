03d2042f305380ab014714bc517ebfa756dbda19
Vamsi <saivamsi7996@gmail.com>
2023-07-02 00:18:30 -0400

    exper_started

--- Portofolio/myPortfolio/src/app/skills/skills.component.ts
+++ Portofolio/myPortfolio/src/app/skills/skills.component.ts
@@ -10,5 +10,11 @@ export class SkillsComponent {
   frontend = true;
   html = 9;
   lang = ['HTML','CSS','Javascript','Bootstrap','Angular','Typescript','RxJs','REGEX','JQuery',
-  'Tailwind','ECMA','ES6','Figma','Adobe Photoshop']
+  'Tailwind','ECMA','ES6','Figma','Adobe Photoshop'];
+  backend = ['Java','Spring','Node.js','Junit', 'Mockito', 'JSP', 'Java Servlets', 'Spring MVC'];
+  testing = ['Karma','Jasmine','JEST', 'Lint','ngtest'];
+  VersionControl = ['Git','CI/CD','BitBucket','Jenkins'];
+  CloudTech = ['Google cloud platform(GCP)','Azure', 'Amazon Web Services'];
+  database = ['SQL','PLLSQL','Oracle DB', 'S3 Bucket storage'];
+  ide=['Visual Studio Code','IntelliJ','Eclipse','Pycharm','Android Studio']
 }
